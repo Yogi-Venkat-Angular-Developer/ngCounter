e8cec276e85952d23a7e8933d2e246c7116f8a62
Yogi Venkat <boppidiyogi@gmail.com>
2025-10-25 02:30:52 +0530

    newp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'incDec';
+  name = "Angular 16 & 17"
+  img = "https://cdn.imgbin.com/12/16/4/imgbin-angular-logo-red-and-white-letter-a-logo-r33rdU6ZgjMm1njGNw6sFsV5b.jpg";
+  topic = "Property-Binding"
+  proImg = "https://www.ifourtechnolab.com/pics/What-is-Property-binding-and-how-to-implement-it-in-Angular.webp"
+  isAvaliable = false;
+  isEven = true;
+  count : number = 0;
+
+checkEvenOdd(){
+  if(this.count % 2 == 1){
+    this.isEven = false
+  }else{
+    this.isEven = true
+  }
 }
+
+  inc(){
+    if(this.count >= 10){
+      this.isAvaliable = true;
+    }else{
+      this.count++
+    }
+}
+  dec(){
+    if(this.count <= 0){
+      this.isAvaliable = true;
+    }else{
+      this.count--
+    }
+  }
+  
+  res(){
+    this.count = 0;
+    location.reload()
+  }
+}
+
---
+++ src/app/readme.md
@@ -0,0 +1,6 @@
+Angular Introduction Basic concepts ;
+
+Angular Topics : Data Binding -> one way data-Binding Two - way data binding ;
+
+
+http://www.w3.org/2000/svg
